61607dd720863894ad895c4a667b5d1fb373218a
Daniel Rendall <drendall@gmail.com>
2022-01-20 22:38:34 +0000

    Document day 8

--- src/main/scala/uk/co/danielrendall/adventofcode/y2021/Day8.scala
+++ src/main/scala/uk/co/danielrendall/adventofcode/y2021/Day8.scala
@@ -7,6 +7,18 @@ import uk.co.danielrendall.adventofcode.utils.StringUtils.*
 
 object Day8 {
 
+  /**
+   * Decoding 7-segment displays. Each line of the test data corresponds to a 7 segment display in which the segments
+   * have letters a-g but we don't know which is which. The first half of the line contains the 10 patterns for digits
+   * 0-9 (but in a random order), the second half contains 4 digits represented by which segments are on.
+   *
+   * Part 1
+   * How many times do the digits 1, 4, 7 or 8 appear (these are the ones which are easy to disambiguate)
+   *
+   * Part 2
+   * Work out all of the four-digit numbers and add them toether
+   *
+   */
   val testData: LazyList[String] =
     """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
       |edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -23,28 +35,40 @@ object Day8 {
   val data: LazyList[String] = this.getClass.getResourceAsStream("/2021/day8.txt").lines.filterNot(_.isEmpty)
 
   @main def d8p1() =
-    def solve(seq: LazyList[String]): Unit =
+    def solve(seq: LazyList[String]): Int =
+      // I actually use the complete solution here, and just filter down to the "easy" digits.
       val decoded: Seq[Seq[Char]] = seq.map(decode)
-      val count = decoded.flatten.count(s => s == '1' || s == '4' || s == '7' || s == '8')
-      println(count)
+      decoded.flatten.count(s => s == '1' || s == '4' || s == '7' || s == '8')
 
 
-    solve(testData)
-    solve(data)
+    println("Test: " +  solve(testData))
+    println("Actual: " + solve(data))
 
   @main def d8p2() =
-    def solve(seq: LazyList[String]): Unit =
+    def solve(seq: LazyList[String]): Int =
       val decoded: Seq[Seq[Char]] = seq.map(decode)
-      val sum = decoded.map(_.mkString.toInt).sum
-      println(sum)
+      decoded.map(_.mkString.toInt).sum
 
 
-    solve(testData)
-    solve(data)
+    println("Test: " +  solve(testData))
+    println("Actual: " + solve(data))
 
+  /**
+   * Given a string of test data, return a sequence of the 4 characters (actually digits) to which it resolves. This
+   * just splits the line into the 10 digit patterns and the mystery digits, calls workOutDigits to get a map from
+   * (set of chars representing switched-on segments) -> resulting digit, then looks up the mystery digits.
+   * @param str
+   * @return
+   */
   def decode(str: String): Seq[Char] =
     val halves = str.split('|').map(_.trim)
+
+    // The set of 10 patterns; they're unordered in the line, so might as well use a set
     val patterns: Set[String] = halves(0).split(' ').toSet
+
+    // A sequence of sets of characters; the sequence will have length 4. Note that the order of the segments as it
+    // appears for a "mystery number" might differ from the order of those segments in the pattern for that number given
+    // in the first half of the line.
     val digitPatterns: Seq[Set[Char]] = halves(1).split(' ').toSeq.map(_.toSet)
     val lookup = workOutDigits(patterns)
     digitPatterns.map(lookup.apply)
@@ -53,6 +77,9 @@ object Day8 {
   // Segment names: t, tl, tr, m, bl, br, b
 
   def workOutDigits(patterns: Set[String]): Map[Set[Char], Char] =
+
+    // First, work out the unambiguous ones e.g. the set of segments representing '1' will have length 2.
+
     // One has 2 segments
     val (oneSet, oneRest) = patterns.partition(_.length == 2)
     // Seven has 3 segments
@@ -61,10 +88,12 @@ object Day8 {
     val (fourSet, fourRest) = sevenRest.partition(_.length == 4)
     // Eight has 7 segments
     val (eightSet, eightRest) = fourRest.partition(_.length == 7)
+
+
     // Remaining: Two, Three, Five (5 segments each), Zero, Six, Nine (6 segments each)
     val (twoThreeFive, zeroSixNine) = eightRest.partition(_.length == 5)
 
-    // These will all work...
+    // These will all work; the sets resulting from the partition should all have length 1
     val one: String = oneSet.head
     val seven: String = sevenSet.head
     val four: String = fourSet.head
