d4d2c8c59b6d48095360a1af2ea202442e00bc09
Daniel Rendall <drendall@gmail.com>
2022-01-20 21:33:22 +0000

    Document day 1

--- src/main/scala/uk/co/danielrendall/adventofcode/y2021/Day1.scala
+++ src/main/scala/uk/co/danielrendall/adventofcode/y2021/Day1.scala
@@ -6,7 +6,19 @@ import uk.co.danielrendall.adventofcode.utils.StringUtils.*
 
 object Day1 {
 
-  val testData: LazyList[String] =
+  /**
+   * Numbers represent depths.
+   *
+   * Part 1
+   * Count how many times a number is greater than the number preceding it.
+   *
+   * Part 2
+   * Group the data into (overlapping) windows of size 3; calculate the sum in each window, and count the number of
+   * times the sum for a window is greater than that of the preceding window
+   * 
+   */
+
+  val testData: LazyList[Int] =
     """199
       |200
       |208
@@ -16,17 +28,23 @@ object Day1 {
       |240
       |269
       |260
-      |263""".stripMargin.splitAndTrimToList
+      |263""".stripMargin.splitAndTrimToList.map(_.toInt)
 
   val numbers: LazyList[Int] = this.getClass.getResourceAsStream("/2021/day1.txt").lines.filterNot(_.isEmpty).map(_.toInt)
 
   @main def d1p1(): Unit =
-    val answer = numbers.zip(numbers.tail).count { case (f, s) => f < s }
-    println(answer)
+    def solve(list: LazyList[Int]) = list.zip(list.tail).count { case (f, s) => f < s }
+
+    println("Test: " +  solve(testData))
+    println("Actual: " + solve(numbers))
 
   @main def d1p2(): Unit =
     val windowSize = 3
-    val windowedList = numbers.windowed(3).map(_.sum)
-    val answer = windowedList.zip(windowedList.tail).count { case (f, s) => f < s }
-    println(answer)
+    def solve(list: LazyList[Int]) = {
+      val windowedList: LazyList[Int] = list.windowed(3).map(_.sum)
+      windowedList.zip(windowedList.tail).count { case (f, s) => f < s }
+    }
+
+    println("Test: " +  solve(testData))
+    println("Actual: " + solve(numbers))
 }
