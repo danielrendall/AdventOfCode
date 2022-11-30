3054c352b39f77610f84d3cca972ddf6ff81c5a8
Daniel Rendall <drendall@gmail.com>
2022-11-30 11:38:03 +0000

    Un-checked-in changes from last year...

--- src/main/scala/uk/co/danielrendall/adventofcode/y2021/Day24.scala
+++ src/main/scala/uk/co/danielrendall/adventofcode/y2021/Day24.scala
@@ -5,6 +5,7 @@ import uk.co.danielrendall.adventofcode.utils.OrderingUtils.*
 import uk.co.danielrendall.adventofcode.utils.StreamUtils.*
 import uk.co.danielrendall.adventofcode.utils.StringUtils.*
 
+import scala.annotation.tailrec
 import scala.util.Random
 import scala.util.matching.Regex
 
@@ -22,6 +23,80 @@ object Day24 {
 
   @main def d24p1() =
     def solve(seq: Seq[String]): Unit =
+      val operations: List[Operation] =
+        parse(seq)
+          .grouped(18)
+          .toList
+          .map(instructions => InstructionGroup(instructions.toList))
+          .map(_.toOperation)
+
+      val ret = for {
+        i3 <- 1 to 9
+        z2 <- for {
+          i2 <- 1 to 9
+          z1 <- for {
+            i1 <- 1 to 9
+          } yield {
+            operations(0).getNextZ(i1, 0)
+          }
+        } yield {
+          operations(1).getNextZ(i2, z1)
+        }
+      } yield {
+        operations(2).getNextZ(i3, z2)
+      }
+      ret.foreach(println)
+
+
+    solve(data)
+
+
+  // Note helpful
+  @main def d24reverseEngineer() =
+    def solve(seq: Seq[String]): Unit =
+      val operations: List[Operation] =
+        parse(seq)
+          .grouped(18)
+          .toList
+          .map(instructions => InstructionGroup(instructions.toList))
+          .map(_.toOperation)
+
+      @tailrec
+      def reverseEngineer(opsInReverseOrder: List[Operation],
+                          targetAccum: List[Map[BigInt, List[InputAndRequiredZ]]]): List[Map[BigInt, List[InputAndRequiredZ]]] =
+        println("Processing")
+        opsInReverseOrder match {
+          case op :: restOps =>
+
+            targetAccum match {
+              case map :: restTarget =>
+                // Map is a map of (z output from previous stage -> combination of inputs and Zs which give that output)
+                // We now need to work out what Zs and inputs to this stage will give us the Zs required at the following
+                // stage (which, confusingly, we have just processed)
+                val allZsToConsider = map.view.values.flatten.map(_.z).toList.sorted
+                val nextMap = allZsToConsider.map(z => z -> investigate(op, z)).toMap
+                reverseEngineer(restOps, nextMap :: targetAccum)
+              case Nil =>
+                val result: List[InputAndRequiredZ] = investigate(op, 0)
+                reverseEngineer(restOps, List(Map(BigInt(0) -> result)))
+            }
+          case Nil => targetAccum.reverse
+        }
+
+
+      val result = reverseEngineer(operations.reverse, List.empty)
+
+      println(result.toString())
+
+
+    solve(data)
+
+
+  /**
+   * This demonstrates that the simplified version in "operation" executes the same algorithm (taking all
+   */
+  @main def d24verify() =
+    def solve(seq: Seq[String]): Unit =
       val allInstructions = parse(seq)
 
       val groups: List[InstructionGroup] =
@@ -34,7 +109,7 @@ object Day24 {
 
       def r = rnd.nextInt(9) + 1
 
-      (0 to 10000).foreach { i =>
+      (0 to 1000).foreach { i =>
         val state = State(0, 0, 0, 0, List(r, r, r, r, r, r, r, r, r, r, r, r, r, r))
 
         val finalState = (operations.zip(groups)).foldLeft(state) { case (thisState, (op, grp)) =>
@@ -45,10 +120,157 @@ object Day24 {
           }
           newOpState
         }
+        val finalState2 = runOnInput(state)
+        if (finalState2.cleaned != finalState.cleaned) {
+          throw new Exception("Final state 2 " + finalState2 + " different from final state " + finalState)
+        }
         println(s"$state -> $finalState")
       }
 
     solve(data)
+
+  @main def d24writeCode() =
+    def solve(seq: Seq[String]): Unit =
+      val allInstructions = parse(seq)
+
+      val groups: List[InstructionGroup] =
+        allInstructions.grouped(18).toList.map(instructions => InstructionGroup(instructions.toList))
+
+      val operations: List[Operation] = groups.map(_.toOperation)
+
+      val code: Seq[String] = operations.zipWithIndex.map { case (op, index) =>
+        op.toScala(index)
+      }
+      code.foreach(println)
+
+
+    solve(data)
+
+
+  def investigate(operation: Operation, requiredZ: BigInt): List[InputAndRequiredZ] = {
+    (1 to 9).flatMap { input =>
+      (-1000000 to 1000000).flatMap { z =>
+        operation.run(State(0, 0, 0, z, List(input))) match {
+          case Some(newState) =>
+            if (newState.z == requiredZ) {
+              Some(InputAndRequiredZ(input, z))
+            } else None
+          case None =>
+            None
+        }
+      }
+    }.toList
+
+  }
+
+  def runOnInput(state: State): State = {
+
+    val input = state.waitingInput
+    val z0 = state.z
+
+    val z1 = if (z0 % 26 + 10 - input(0) == 0) {
+      z0 / 1
+    } else {
+      26 * (z0 / 1) + input(0) + 10
+    }
+
+
+    val z2 = if (z1 % 26 + 13 - input(1) == 0) {
+      z1 / 1
+    } else {
+      26 * (z1 / 1) + input(1) + 5
+    }
+
+
+    val z3 = if (z2 % 26 + 15 - input(2) == 0) {
+      z2 / 1
+    } else {
+      26 * (z2 / 1) + input(2) + 12
+    }
+
+
+    val z4 = if (z3 % 26 - 12 - input(3) == 0) {
+      z3 / 26
+    } else {
+      26 * (z3 / 26) + input(3) + 12
+    }
+
+
+    val z5 = if (z4 % 26 + 14 - input(4) == 0) {
+      z4 / 1
+    } else {
+      26 * (z4 / 1) + input(4) + 6
+    }
+
+
+    val z6 = if (z5 % 26 - 2 - input(5) == 0) {
+      z5 / 26
+    } else {
+      26 * (z5 / 26) + input(5) + 4
+    }
+
+
+    val z7 = if (z6 % 26 + 13 - input(6) == 0) {
+      z6 / 1
+    } else {
+      26 * (z6 / 1) + input(6) + 15
+    }
+
+
+    val z8 = if (z7 % 26 - 12 - input(7) == 0) {
+      z7 / 26
+    } else {
+      26 * (z7 / 26) + input(7) + 3
+    }
+
+
+    val z9 = if (z8 % 26 + 15 - input(8) == 0) {
+      z8 / 1
+    } else {
+      26 * (z8 / 1) + input(8) + 7
+    }
+
+
+    val z10 = if (z9 % 26 + 11 - input(9) == 0) {
+      z9 / 1
+    } else {
+      26 * (z9 / 1) + input(9) + 11
+    }
+
+
+    val z11 = if (z10 % 26 - 3 - input(10) == 0) {
+      z10 / 26
+    } else {
+      26 * (z10 / 26) + input(10) + 2
+    }
+
+
+    val z12 = if (z11 % 26 - 13 - input(11) == 0) {
+      z11 / 26
+    } else {
+      26 * (z11 / 26) + input(11) + 12
+    }
+
+
+    val z13 = if (z12 % 26 - 12 - input(12) == 0) {
+      z12 / 26
+    } else {
+      26 * (z12 / 26) + input(12) + 4
+    }
+
+
+    val z14 = if (z13 % 26 - 13 - input(13) == 0) {
+      z13 / 26
+    } else {
+      26 * (z13 / 26) + input(13) + 11
+    }
+
+    State(0, 0, 0, z14, List.empty)
+
+  }
+
+  case class InputAndRequiredZ(input: Int, z: BigInt)
+
 
   sealed trait CalculationStep {
     // Run attempts to consume an item from the state's input list and returns None if
@@ -70,15 +292,32 @@ object Day24 {
         case _ :: rest =>
           val w: BigInt = state.waitingInput.head
           val z: BigInt = state.z
-
-          val newZ = if (z % 26 + v2 == w) {
-            z / v1
-          } else {
-            26 * (z / v1) + (w + v3)
-          }
+          val newZ = getNextZ(w, z)
           Some(State(0, 0, 0, newZ, rest))
         case Nil => None
       }
+    }
+
+    def getNextZ(w: BigInt, z: BigInt) = {
+      if (z % 26 + v2 - w == 0) {
+        z / v1
+      } else {
+        26 * (z / v1) + (w + v3)
+      }
+    }
+
+    def toScala(index: Int): String = {
+      val thisZ = "z" + (index + 1)
+      val lastZ = "z" + index
+      val input = "input(" + index + ")"
+      s"""val $thisZ = if ($lastZ % 26 ${if (v2 >= 0) s"+ $v2" else s"$v2"} - $input == 0) {
+         |  $lastZ / $v1
+         |} else {
+         |  26 * ($lastZ / $v1) + $input + $v3
+         |}
+         |
+         |""".stripMargin
+
     }
   }
 
