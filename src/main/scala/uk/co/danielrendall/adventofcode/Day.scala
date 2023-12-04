aea1eb04727766167350f888e1b93c6a71671dc5
Daniel Rendall <drendall@gmail.com>
2023-12-04 07:39:21 +0000

    Blank day for copying and pasting

---
+++ src/main/scala/uk/co/danielrendall/adventofcode/Day.scala
@@ -0,0 +1,29 @@
+package uk.co.danielrendall.adventofcode
+
+import uk.co.danielrendall.adventofcode.utils.StreamUtils.*
+import uk.co.danielrendall.adventofcode.utils.StringUtils.*
+
+object Day {
+
+  val testData: LazyList[String] =
+    """""".stripMargin.splitAndTrimToList
+
+  val numbers: LazyList[String] = this.getClass.getResourceAsStream("/202x/day.txt").lines
+
+  @main def dxp1(): Unit = {
+    def solve(list: LazyList[String]) = ""
+
+
+    println("Test: " + solve(testData))
+    println("Actual: " + solve(numbers))
+  }
+
+  @main def dxp2(): Unit = {
+
+    def solve(list: LazyList[String]) = ""
+
+    println("Test: " + solve(testData))
+    println("Actual: " + solve(numbers))
+  }
+}
+
