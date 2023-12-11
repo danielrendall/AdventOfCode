c4df7d326e98e60fda6104a43101bf1bff60d24b
Daniel Rendall <drendall@gmail.com>
2023-12-11 09:39:09 +0000

    Day 11 of the advent calendar completed

--- src/main/scala/uk/co/danielrendall/adventofcode/y2023/Day11.scala
+++ src/main/scala/uk/co/danielrendall/adventofcode/y2023/Day11.scala
@@ -26,25 +26,8 @@ object Day11 {
   @main def d11p1(): Unit = {
     def solve(list: LazyList[String]) = {
       val grid = ArrayUtils.buildBorderedArray(list, identity, '.')
-      val expanded = expand(grid)
-      val locs = expanded.findLocs(_ == '#').toList
-      @tailrec
-      def findAllDistances(remaining: List[Loc], count: Int): Int =
-        remaining match {
-          case head :: rest =>
-            val newCount = rest.foldLeft(count) { case (count, otherLoc) =>
-              val yDiff = Math.abs(otherLoc.y - head.y)
-              val xDiff = Math.abs(otherLoc.x - head.x)
-              count + xDiff + yDiff
-            }
-            findAllDistances(rest, newCount)
-
-          case _ =>
-            count
-        }
-      findAllDistances(locs, 0)
+      solveWithExpansionFactor(grid, 2)
     }
-
 
     println("Test: " + solve(testData))
     println("Actual: " + solve(numbers))
@@ -52,28 +35,50 @@ object Day11 {
 
   @main def d11p2(): Unit = {
 
-    def solve(list: LazyList[String]) = ""
+    def solve(list: LazyList[String]) = {
+      val grid = ArrayUtils.buildBorderedArray(list, identity, '.')
+      solveWithExpansionFactor(grid, 1000000)
+    }
 
     println("Test: " + solve(testData))
     println("Actual: " + solve(numbers))
   }
 
-  private def expand(grid: Array2D[Char]): Array2D[Char] = {
-    val blankRows = grid.rowIteratorLR
+  private def solveWithExpansionFactor(grid: Array2D[Char],
+                                       expansionFactor: Int) = {
+    val locs = grid.findLocs(_ == '#').toList
+    val blankRowIndexes: List[Int] = grid.rowIteratorLR
       .zipWithIndex
-      .filter { case (rowIt, _) => rowIt.forall(l => grid.get(l) == '.')}
+      .filter { case (rowIt, _) => rowIt.forall(l => grid.get(l) == '.') }
       .map { case (_, idx) => idx + 1 }
       .toList
-      .reverse
-
-    val blankCols = grid.columnIteratorTD
+    val blankColumnIndexes: List[Int] = grid.columnIteratorTD
       .zipWithIndex
       .filter { case (colIt, _) => colIt.forall(l => grid.get(l) == '.') }
       .map { case (_, idx) => idx + 1 }
       .toList
-      .reverse
-    val expandedWithRows = blankRows.foldLeft(grid) { case (grid, idx) => grid.insertRow(idx, '.')}
-    blankCols.foldLeft(expandedWithRows) { case (grid, idx) => grid.insertCol(idx, '.')}
+
+    @tailrec
+    def findAllDistances(remaining: List[Loc], count: Long): Long =
+      remaining match {
+        case head :: rest =>
+          val newCount = rest.foldLeft(count) { case (count, otherLoc) =>
+            val xMax = Math.max(otherLoc.x, head.x)
+            val xMin = Math.min(otherLoc.x, head.x)
+            val yMax = Math.max(otherLoc.y, head.y)
+            val yMin = Math.min(otherLoc.y, head.y)
+            val xAdditions = blankColumnIndexes.count(idx => idx > xMin && idx < xMax) * (expansionFactor - 1)
+            val yAdditions = blankRowIndexes.count(idx => idx > yMin && idx < yMax) * (expansionFactor - 1)
+            count + (xMax - xMin + xAdditions) + (yMax - yMin + yAdditions)
+          }
+          findAllDistances(rest, newCount)
+
+        case _ =>
+          count
+      }
+
+    findAllDistances(locs, 0L)
+
   }
 }
 
